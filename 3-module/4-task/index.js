21a77e869b29e5c3aaf40d82b47a4ec53697c76d
Tihon Filippov <t.filippov@adcamp.ru>
2022-06-02 11:24:15 +0300

    Сделал все задачи из 3-го модуля

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+   return users
+      .filter((item) => item.age <= age) // фильтруем по дате
+      .map((item) => `${item.name}, ${item.balance}`) // забираем нейм и зп
+      .join("\n"); // массив в строку + перенос
 }
