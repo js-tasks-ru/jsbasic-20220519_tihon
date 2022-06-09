5c2508c9e152917fac1ea079f09a2b65ed20ddb7
Tihon Filippov <t.filippov@adcamp.ru>
2022-06-09 18:04:18 +0300

    Сделал модуль 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+  document.body.append(ul);
+
+  for (let i = 0; i < friends.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = friends[i].firstName + " " + friends[i].lastName;
+    ul.append(li);
+  }
+  return ul;
 }
