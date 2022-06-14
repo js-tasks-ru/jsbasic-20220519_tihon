4be33a676adc57323094ad89324c957233087bcf
Tihon Filippov <t.filippov@adcamp.ru>
2022-06-14 11:03:23 +0300

    реализация кнопок для слайдера

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -8,37 +8,45 @@ function initCarousel() {
   let sliderWidth = carouselInner.offsetWidth;
   let position = 0;
   let counter = 0;
-
+  
   btnLeft.style.display = 'none';
 
   btnRight.addEventListener("click", () => { 
     position -= sliderWidth;
     counter++;
 
-    position = Math.max(position, -sliderWidth * (carouselSlide.length -1));
-    carouselInner.style.transform = "translateX(" + position + "px)";
-    console.log(counter);
-
-    if (counter >= carouselSlide.length -1) {
-      btnRight.style.display = 'none';
-    } else {
-      btnRight.style.display = '';
+    if (counter > 0) {
+      btnLeft.style.display = '';
     }
 
+    if (counter == carouselSlide.length -1) {
+      btnRight.style.display = 'none';
+    }
+
+    position = Math.max(position, -sliderWidth * (carouselSlide.length -1));
+    carouselInner.style.transform = "translateX(" + position + "px)";
+
+    console.log(counter);
+    
+    
   });
 
   btnLeft.addEventListener("click", () => {
     position += sliderWidth;
     counter--;
 
+    if (counter == 0) {
+      btnLeft.style.display = 'none';
+    }
+
+    if (counter < carouselSlide.length -1) {
+      btnRight.style.display = '';
+    }
+
     position = Math.min(position, 0);
     carouselInner.style.transform = "translateX(" + position + "px)";
-    console.log(counter);
 
-    if (counter <= 0) {
-      btnLeft.style.display = 'none';
-    } else {
-      btnLeft.style.display = '';
-    }
+    console.log(counter);
+    
   });
 }
